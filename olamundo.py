7a1c045cd9d1cd54f6ef4f35c64a58a849e80a0c
Gil <gilpneus3@hotmail.com>
2025-05-07 09:55:22 -0300

    atividade

---
+++ olamundo.py
@@ -0,0 +1,25 @@
+import string
+
+from random import *
+
+def numeroAcertos(lista,gabarito):
+    cont = 0
+    for i in range(len(gabarito)):
+        if lista[i] == gabarito[i]:
+            cont +=1
+    
+    return lista,cont
+
+
+gabarito = [choice(string.ascii_uppercase[:5]) for i in range(10)]
+
+dic_respostas_aluno = {}
+
+for i in range(20):
+    dic_respostas_aluno[i] = numeroAcertos([choice(string.ascii_uppercase[:5]) for i in range(10)],gabarito)
+
+for chave,valor in dic_respostas_aluno.items():
+    print(f'{chave}: {valor}')
+
+
+
